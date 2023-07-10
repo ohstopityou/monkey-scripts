0705b5fcce84f531600a7ede7cd5a68da58ee01a
Thomas Søvik <thomasovik@gmail.com>
2023-07-11 00:15:14 +0200

    Changed to tampermonkey

---
+++ video-control.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         BetterVideoControls
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Improves video controls by setting 'space' -> play/pause and 'enter' to toggle fullscreen
+// @author       Thomas Sovik
+// @match        *://*.youtube.com/watch*
+// @match        *://*.twitch.tv/*
+// @match        *://*.linkedin.com/*
+// @require      file:///Users/thomassovik/GitHub/monkey-scripts/video-control.js
+
+// ==/UserScript==
+
+let domain = window.location.hostname.split('.')[1]
+if (domain) {
+	console.log(`BetterVideoControl Enabled on ${domain}`)
+}
+
+// TODO: If going from youtube.com to /watch, script does not run.
