20cd3acf9023f2e6f722a6d19d394d08f79147a2
Thomas Søvik <thomasovik@gmail.com>
2023-07-10 22:09:55 +0200

    added monkey template

---
+++ video-control.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 10/07/2023, 21:44:10
+// ==/UserScript==
+
+console.log('yeyee')
